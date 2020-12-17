8686d181e886a88a053a7a4d650a6d80b7f1dc75
Navya Singla <60549106+Navyasingla1309@users.noreply.github.com>
2020-12-17 14:21:44 +0530

    Update emailsender.py

--- emailsender.py
+++ emailsender.py
@@ -9,7 +9,7 @@ email = EmailMessage()
 email['from'] = 'person1'
 email['to'] = 'abc@gmail.com'
 email['subject']= 'CONGRATULATIONS! YOU JUST WON $10000000 CASH'
-email.set_content(html.substitute({'name': 'pmon'}),'html')
+email.set_content(html.substitute({'name': 'abc'}),'html')
 
 with smtplib.SMTP(host ='smtp.gmail.com', port =587) as smtp:
     smtp.ehlo()
